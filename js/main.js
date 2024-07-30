c329b6fb9db07b95cd177e93bf40a05c7fde54b4
Sasha <sashahayles@yahoo.com>
2024-07-29 20:10:40 -0400

    cleaned up functions

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ const xhr = new XMLHttpRequest();
 xhr.withCredentials = false;
 
 xhr.addEventListener('readystatechange', function () {
-if (this.readyState === this.DONE) {
-console.log(this.responseText);
-}
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
 });
 
 xhr.open('GET', 'https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P436359&preferedSku=2131803');
@@ -31,159 +31,150 @@ function results(event) {
   let xhttpEight = new XMLHttpRequest();
   let xhttpNine = new XMLHttpRequest();
 
-event.preventDefault();
-xhttpThree.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
+  event.preventDefault();
+  var formcheckA = document.getElementById("a-question");
+  var formcheckB = document.getElementById("b-question");
+  var formcheckC = document.getElementById("c-question");
+  var formcheckD = document.getElementById("d-question");
+  var formcheckE = document.getElementById("e-question");
+  var formcheckF = document.getElementById("f-question");
 
-if(formcheckA.checked && formcheckD.checked == true) {
-response = JSON.parse(xhttpThree.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpTwo.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
+console.log(formcheckA.checked);
+    if(formcheckA.checked) {
+      if(formcheckD.checked) {
+        xhttpThree.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P443833&preferedSku=2211605", true);
+        xhttpThree.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+        xhttpThree.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+        xhttpThree.send(data);
 
-if(formcheckA.checked && formcheckE.checked == true) {
-response = JSON.parse(xhttpTwo.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpFour.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckA.checked && formcheckF.checked == true) {
-response = JSON.parse(xhttpFour.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpFive.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckB.checked && formcheckD.checked == true) {
-response = JSON.parse(xhttpFive.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpSix.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckB.checked && formcheckE.checked == true) {
-response = JSON.parse(xhttpSix.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpSeven.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckB.checked && formcheckF.checked == true) {
-response = JSON.parse(xhttpSeven.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttp.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckC.checked && formcheckD.checked == true) {
-response = JSON.parse(xhttp.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpEight.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckC.checked && formcheckE.checked == true) {
-response = JSON.parse(xhttpEight.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttpNine.onreadystatechange = function() {
-var formcheckA = document.getElementById("a-question");
-var formcheckB = document.getElementById("b-question");
-var formcheckC = document.getElementById("c-question");
-var formcheckD = document.getElementById("d-question");
-var formcheckE = document.getElementById("e-question");
-var formcheckF = document.getElementById("f-question");
-if(formcheckC.checked && formcheckF.checked == true) {
-response = JSON.parse(xhttpNine.response);
-document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
-}
-}
-xhttp.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P431187&preferedSku=2046597", true);
-xhttp.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttp.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttp.send(data);
+        xhttpThree.onreadystatechange = function() {
+          console.log(xhttpThree.response);
+          response = JSON.parse(xhttpThree.response);
+          document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+        }
+      }
+    }
 
-xhttpTwo.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P411387&preferedSku=1863588", true);
-xhttpTwo.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpTwo.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpTwo.send(data);
+  if(formcheckA.checked) {
+    if(formcheckE.checked) {
+      xhttpTwo.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P411387&preferedSku=1863588", true);
+      xhttpTwo.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpTwo.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpTwo.send(data);
 
-xhttpThree.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P443833&preferedSku=2211605", true);
-xhttpThree.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpThree.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpThree.send(data);
+      xhttpTwo.onreadystatechange = function() {
+        console.log(xhttpTwo.response);
+        response = JSON.parse(xhttpTwo.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckA.checked) {
+    if(formcheckF.checked) {
+      xhttpFour.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P474937&preferedSku=2491413", true);
+      xhttpFour.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpFour.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpFour.send(data);
 
-xhttpFour.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P474937&preferedSku=2491413", true);
-xhttpFour.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpFour.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpFour.send(data);
+      xhttpFour.onreadystatechange = function() {
+        console.log(xhttpFour.response);
+        response = JSON.parse(xhttpFour.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckB.checked) {
+    if(formcheckD.checked) {
+      xhttpFive.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P461537&preferedSku=2382232", true);
+      xhttpFive.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpFive.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpFive.send(data);
 
-xhttpFive.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P461537&preferedSku=2382232", true);
-xhttpFive.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpFive.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpFive.send(data);
+      xhttpFive.onreadystatechange = function() {
+        console.log(xhttpFive.response);
+        response = JSON.parse(xhttpFive.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckB.checked) {
+    if(formcheckE.checked) {
+      xhttpSix.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P500618&preferedSku=2581601", true);
+      xhttpSix.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpSix.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpSix.send(data);
 
-xhttpSix.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P500618&preferedSku=2581601", true);
-xhttpSix.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpSix.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpSix.send(data);
+      xhttpSix.onreadystatechange = function() {
+        console.log(xhttpSix.response);
+        response = JSON.parse(xhttpSix.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckB.checked) {
+    if(formcheckF.checked) {
+      xhttpSeven.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P500614&preferedSku=2529675", true);
+      xhttpSeven.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpSeven.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpSeven.send(data);
 
-xhttpSeven.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P500614&preferedSku=2529675", true);
-xhttpSeven.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpSeven.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpSeven.send(data);
+      xhttpSeven.onreadystatechange = function() {
+        console.log(xhttpSeven.response);
+        response = JSON.parse(xhttpSeven.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckC.checked) {
+    if(formcheckD.checked) {
+      xhttp.open('GET', 'https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P512265&preferedSku=2788222');
+      xhttp.setRequestHeader('x-rapidapi-key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttp.setRequestHeader('x-rapidapi-host', 'sephora.p.rapidapi.com');
+      xhttp.send(data);
 
-xhttpEight.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P503634&preferedSku=2661239", true);
-xhttpEight.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpEight.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpEight.send(data);
+      xhttp.onreadystatechange = function() {
+        console.log(xhttp.response);
+        response = JSON.parse(xhttp.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckC.checked) {
+    if(formcheckE.checked) {
+      xhttpEight.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P503634&preferedSku=2661239", true);
+      xhttpEight.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpEight.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpEight.send(data);
 
-xhttpNine.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P469520&preferedSku=2421196", true);
-xhttpNine.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
-xhttpNine.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
-xhttpNine.send(data);
+      xhttpEight.onreadystatechange = function() {
+        console.log(xhttpEight.response);
+        response = JSON.parse(xhttpEight.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if(formcheckC.checked) {
+    if(formcheckF.checked) {
+      xhttpNine.open("GET", "https://sephora.p.rapidapi.com/us/products/v2/detail?productId=P469520&preferedSku=2421196", true);
+      xhttpNine.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+      xhttpNine.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+      xhttpNine.send(data);
+
+      xhttpNine.onreadystatechange = function() {
+        console.log(xhttpNine.response);
+        response = JSON.parse(xhttpNine.response);
+        document.getElementById("900098").innerHTML = response.content.seoTitle + " " + response.content.seoMetaDescription;
+      }
+    }
+  }
+  if (formcheckD.checked) {
+    xhttp.open("GET", "http://demo3919838.mockable.io/", true);
+    xhttp.send(data);
+
+    xhttp.onreadystatechange = function() {
+      response = JSON.parse(xhttp.response);
+      console.log(response.userNote[0].type);
+      document.getElementById("900098").innerHTML = response.userNote[0].type;
+    }
+  }
 }
