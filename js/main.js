13d88023f0d8cff2d6063d6e900df26a0088180b
Sasha <sashahayles@yahoo.com>
2024-07-30 13:45:24 -0400

    changed http to https

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ function results(event) {
   }
   if(formcheckC.checked) {
     if(formcheckF.checked) {
-      xhttpNine.open("GET", "http://demo3919838.mockable.io/thanks/", true);
+      xhttpNine.open("GET", "https://demo3919838.mockable.io/thanks", true);
     //  xhttpNine.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
       //xhttpNine.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
       xhttpNine.send(data);
