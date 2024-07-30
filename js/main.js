f8b4545c36436f6893df2e058eb3c7ccb0836b11
Sasha <sashahayles@yahoo.com>
2024-07-30 14:45:03 -0400

    database is now connected to product button and working

--- js/main.js
+++ js/main.js
@@ -180,4 +180,16 @@ function results(event) {
       document.getElementById("900099").innerHTML = response.userNote[0].type;
     }
   }
+  if(resetBtn.form) {
+    xhttpTen.open("GET", "https://demo3919838.mockable.io/thanks", true);
+    //  xhttpNine.setRequestHeader('X-RapidAPI-Key', '3c847760d5mshe01e3c882569eafp1ae715jsnbdcf5c7796ef');
+    //xhttpNine.setRequestHeader('X-RapidAPI-Host', 'sephora.p.rapidapi.com');
+    xhttpTen.send(data);
+
+    xhttpTen.onreadystatechange = function() {
+      response = JSON.parse(xhttpTen.response);
+      console.log(response.userNote[1].type);
+      document.getElementById("900099").innerHTML = response.userNote[1].type;
+    }
+  }
 }
